ff89ef007fab5182665a26a1df7cad8badbc7d0e
Gaurang Tandon <1gaurangtandon@gmail.com>
2019-02-17 09:22:13 +0530

    prepping for ITSS: split pages; navbar component separated; added favicon

---
+++ js/nav.js
@@ -0,0 +1,49 @@
+(function() {
+	var navInnerHTML = `
+    <a class="navbar-brand" href="#">Gaurang</a>
+    <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+    >
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="index.html">Home</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="skills.html">Skills</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="projects.html">Projects</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="about.html">About me</a>
+            </li>
+        </ul>
+    </div>`;
+
+	window.onload = function() {
+		var navbar = document.createElement("div"),
+			screenReaderText = " <span class=\"sr-only\">(current)</span>";
+		navbar.className = "navbar navbar-expand-lg navbar-dark bg-dark";
+		navbar.innerHTML = navInnerHTML;
+		document.body.insertBefore(navbar, document.body.firstChild);
+
+		var allLinks = document.querySelectorAll("nav-item");
+		for (let i = 0, link; i < allLinks.length; i++) {
+			link = allLinks[i];
+			if (new RegExp(link.href).test(window.location.href)) {
+				link.innerHTML += screenReaderText;
+				break;
+			}
+		}
+	};
+})();
